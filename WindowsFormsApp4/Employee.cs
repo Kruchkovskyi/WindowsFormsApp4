351eab52b4f4a0619fa870618602f087e0e2aab7
User <User@DESKTOP-R9G6CTQ>
2024-04-25 21:32:19 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    internal class Employee
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public bool Programmer { get; private set; }
+        public Employee(string name, int age, bool programmer)
+        {
+            Name = name;
+            Age = age;
+            Programmer = programmer;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
